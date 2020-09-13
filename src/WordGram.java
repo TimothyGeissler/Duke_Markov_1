684a8688312fdff98d53bb30fe99774d71e7dd7d
TimothyGeissler <timothy.geissler@duke.edu>
2020-09-12 21:34:22 -0400

    Working WordGram class - 5/5

--- src/WordGram.java
+++ src/WordGram.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A WordGram represents a sequence of strings
  * just as a String represents a sequence of characters
  * 
- * @author YOUR NAME HERE
+ * @author T Geissler
  *
  */
 public class WordGram {
@@ -24,7 +26,10 @@ public class WordGram {
 		myToString = null;
 		myHash = 0;
 
-		// TODO: initialize all instance variable
+		for (int i = 0; i < size; i++) {
+			myWords[i] = source[start + i];
+		}
+
 	}
 
 	/**
@@ -40,33 +45,54 @@ public class WordGram {
 	}
 
 	/**
-	 * Complete this comment
-	 * @return
+	 *
+	 * @return length of String array myWords of the WordGram object
 	 */
 	public int length(){
-		// TODO: change this
-		return 0;
+		return this.myWords.length;
 	}
 
 
 	/**
-	 * Complete appropriate comment here
-	 * @param o
-	 * @return
+	 *
+	 * @param o the object to be compared to
+	 * @return boolean if o is WordGram object, same length & same words in myWords @ same indices
 	 */
 	@Override
 	public boolean equals(Object o) {
-		if (! (o instanceof WordGram) || o == null){
+		if (!(o instanceof WordGram) || o == null){
 			return false;
+		} else {
+			WordGram wg = (WordGram) o;
+			boolean isEquals = true;
+			if (this.myWords.length != wg.myWords.length) {
+				isEquals = false;
+			} else {
+				for (int i = 0; i < wg.myWords.length; i++) {
+					if (!this.myWords[0].equals(wg.myWords[0])) {
+						isEquals = false;
+					}
+				}
+			}
+			if (!isEquals) {
+				return false;
+			} else {
+				return true;
+			}
 		}
-		// TODO: Complete this method
-
-		return true;
 	}
 
+	/**
+	 *
+	 * @return hashcode derived from toString of object
+	 */
 	@Override
 	public int hashCode(){
-		// TODO: complete this method: assign to myHash as needed
+		int myHash = 0;
+		if (this.myHash == 0) {
+			System.out.println("Calculating hashcode");
+			myHash = this.toString().hashCode();
+		}
 		return myHash;
 	}
 	
@@ -74,18 +100,32 @@ public class WordGram {
 	/**
 	 * Create and complete this comment
 	 * @param last is last String of returned WordGram
-	 * @return
+	 * @return WordGRam object with elements shifted left & last param added
 	 */
 	public WordGram shiftAdd(String last) {
-		WordGram wg = new WordGram(myWords,0,myWords.length);
-		// TODO: Complete this method
-
+		System.out.println("last: " + last);
+		System.out.println("Array to modify: " + Arrays.toString(myWords));
+		String [] newWords = new String[myWords.length + 1];
+		for (int i = 0; i < myWords.length; i++) {
+			newWords[i] = myWords[i];
+		}
+		newWords[myWords.length] = last;
+		System.out.println("New array: " + Arrays.toString(newWords));
+		WordGram wg = new WordGram(newWords,1,newWords.length - 1);
 		return wg;
 	}
 
+	/**
+	 * Only triggers once, then stores in variable myToString
+	 * @return String of combined items in myWords array of this object
+	 */
 	@Override
 	public String toString(){
-		// TODO: Complete this method, assign to myToString as needed
+		if (this.myToString == null) {
+			System.out.println("calculating toString...");
+			myToString = String.join(" ", this.myWords);
+		}
+
 		return myToString;
 	}
 }
